d946dee06919874fe7a5177d735e80fceb706e5e
TinyCourses <tinycourses@gmail.com>
2015-10-28 23:13:21 -0700

    ~ Rename. Tweak comment.

--- src/org/tinycourses/javatutorial/access_modifier_on_class/MyExperiment.java
+++ src/org/tinycourses/javatutorial/access_modifier_on_class/MyExperiment.java
@@ -4,16 +4,13 @@ import org.tinycourses.javatutorial.access_modifier_on_class.package1.MyClass;
 import org.tinycourses.javatutorial.access_modifier_on_class.package2.subpackage.MySubpackageClass;
 
 /**
- * Created with IntelliJ IDEA.
  * Author: Tony Crusoe <tinyCourses@gmail.com>404
- * Date: 3/12/14
- * Time: 6:10 PM
- * To change this template use File | Settings | File Templates.
+ * Date: 2014-03-12T18:10-0700
  */
 public class MyExperiment {
     public static void main (String[] args){
         MyClass myclass = new MyClass();
         MySubpackageClass mySubpackageClass = new MySubpackageClass();
-        System.out.println("MyExperiment done");
+        System.out.println("ArrayExperiment done");
     }
 }
